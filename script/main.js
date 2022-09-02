1f6715b3795caa5b3066025dc6bcd695310ef167
ArtemButovych <artembutovych@gmail.com>
2022-09-02 13:57:57 +0300

    first commit

---
+++ script/main.js
@@ -0,0 +1,17 @@
+'use srtict'
+
+let people  = confirm('Are you people?')
+console.log(people);
+
+let nameUser = prompt('What is your name', '');
+
+let surnameUser = prompt('What is your surname', '');
+
+let ageUser = prompt('How old are you', '');
+
+let liveUser = prompt('Where are do you live', '');
+
+console.log(`Hello ${nameUser}`);
+console.log(`Your surname ${surnameUser}`);
+console.log(`Your age ${ageUser}`);
+console.log(`You live ${liveUser}`);
